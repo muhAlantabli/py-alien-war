4f7bf63d09c2b465741c28ebaeb2d922b8aca93a
Muhammad Alantabli <muhalantabli@gmail.com>
2019-03-03 14:39:10 +0200

    change background color

--- main.py
+++ main.py
@@ -11,12 +11,18 @@ def run_game():
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien War")
 
+    # Set the background color (light grey)
+    bg_color = (230, 230, 230)
+
     # The main loop of the game
     while True:
         # Watch the keyboard and mouse events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        # Fill the screen with given color
+        screen.fill(bg_color)
 
         # Update the screen surface
         pygame.display.flip()
